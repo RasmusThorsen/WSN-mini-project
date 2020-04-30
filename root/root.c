8ed294f25ad2a32ec11e7d7bc9d24df3ba2500e3
Alexander <a-m-h-r@hotmail.com>
2020-04-30 20:27:58 +0200

    Works again

--- root/root.c
+++ root/root.c
@@ -1,7 +1,7 @@
 #include "contiki.h"
 #include "rpl.h"
 #include "httpd-simple.h"
-#include "net/ipv6/simple-udp.h"
+// #include "net/ipv6/simple-udp.h"
 #include <stdio.h>
 // #include "net/routing/routing.h"
 // #include "net/netstack.h"
@@ -9,7 +9,7 @@
 #include "../shared/defs.h"
 
 #include "sys/log.h"
-#define LOG_MODULE "Root"
+#define LOG_MODULE "Web sense"
 #define LOG_LEVEL LOG_LEVEL_INFO
 #define BUFFER_SIZE 50
 
@@ -20,96 +20,96 @@
 
 /*---------------------------------------------------------------------------*/
 // Defined callback, that is setup in simple_udp_register
-static struct simple_udp_connection udp_conn;
-static void data_receiver(
-    struct simple_udp_connection *c,
-    const uip_ipaddr_t *sender_addr,
-    uint16_t sender_port,
-    const uip_ipaddr_t *receiver_addr,
-    uint16_t receiver_port,
-    const uint8_t *data,
-    uint16_t datalen)
-{
-  printf("received data at ROOT: %s\n", (char *)data);
+// static struct simple_udp_connection udp_conn;
+// static void data_receiver(
+//     struct simple_udp_connection *c,
+//     const uip_ipaddr_t *sender_addr,
+//     uint16_t sender_port,
+//     const uip_ipaddr_t *receiver_addr,
+//     uint16_t receiver_port,
+//     const uint8_t *data,
+//     uint16_t datalen)
+// {
+//   printf("received data at ROOT: %s\n", (char *)data);
 
-  // char temp_name[UIPLIB_IPV6_MAX_STR_LEN];
-  // uiplib_ipaddr_snprint(temp_name, sizeof(temp_name), sender_addr);
-  // // sprintf(ip_of_mote, "%s", remove_colon(ip_of_mote));
+//   // char temp_name[UIPLIB_IPV6_MAX_STR_LEN];
+//   // uiplib_ipaddr_snprint(temp_name, sizeof(temp_name), sender_addr);
+//   // // sprintf(ip_of_mote, "%s", remove_colon(ip_of_mote));
 
-  // bool fileExists = false;
-  // int index_of_name = -1;
-  // for(i = 0; i < MAX_NUMBER_AGGREGATORS; i++)
-  // {
-  //   if(strcmp(names[i], remove_colon(temp_name)) == 0)
-  //   {
-  //     fileExists = true;
-  //     index_of_name = i;
-  //     break;
-  //   }
-  // }
+//   // bool fileExists = false;
+//   // int index_of_name = -1;
+//   // for(i = 0; i < MAX_NUMBER_AGGREGATORS; i++)
+//   // {
+//   //   if(strcmp(names[i], remove_colon(temp_name)) == 0)
+//   //   {
+//   //     fileExists = true;
+//   //     index_of_name = i;
+//   //     break;
+//   //   }
+//   // }
 
-  // if (!fileExists)
-  // {
-  //   snprintf(names[next_free_name], sizeof(temp_name), "%s", remove_colon(temp_name));
-  //   index_of_name = next_free_name;
-  //   next_free_name++;
-  // }
+//   // if (!fileExists)
+//   // {
+//   //   snprintf(names[next_free_name], sizeof(temp_name), "%s", remove_colon(temp_name));
+//   //   index_of_name = next_free_name;
+//   //   next_free_name++;
+//   // }
 
-  // int fd = cfs_open(names[index_of_name], CFS_WRITE | CFS_APPEND);
-  // if(fd < 0)
-  // {
-  //   printf("Error opening file \n");
-  // }
-  // else
-  // {
-  //   int e = cfs_write(fd, data, datalen);
-  //   if (e < 0)
-  //   {
-  //     printf("Errpr writing to file \n");
-  //   }
-  // }
+//   // int fd = cfs_open(names[index_of_name], CFS_WRITE | CFS_APPEND);
+//   // if(fd < 0)
+//   // {
+//   //   printf("Error opening file \n");
+//   // }
+//   // else
+//   // {
+//   //   int e = cfs_write(fd, data, datalen);
+//   //   if (e < 0)
+//   //   {
+//   //     printf("Errpr writing to file \n");
+//   //   }
+//   // }
   
-  // cfs_close(fd);
-}
+//   // cfs_close(fd);
+// }
 /*---------------------------------------------------------------------------*/
-static struct simple_udp_connection event_conn;
-static void event_receiver(
-    struct simple_udp_connection *c,
-    const uip_ipaddr_t *sender_addr,
-    uint16_t sender_port,
-    const uip_ipaddr_t *receiver_addr,
-    uint16_t receiver_port,
-    const uint8_t *data,
-    uint16_t datalen)
-{
-    LOG_INFO("Root Received EVENT '%.*s' from ", datalen, (char *)data);
-    LOG_INFO_6ADDR(sender_addr);
-    LOG_INFO_("\n");
-}
+// static struct simple_udp_connection event_conn;
+// static void event_receiver(
+//     struct simple_udp_connection *c,
+//     const uip_ipaddr_t *sender_addr,
+//     uint16_t sender_port,
+//     const uip_ipaddr_t *receiver_addr,
+//     uint16_t receiver_port,
+//     const uint8_t *data,
+//     uint16_t datalen)
+// {
+//     LOG_INFO("Root Received EVENT '%.*s' from ", datalen, (char *)data);
+//     LOG_INFO_6ADDR(sender_addr);
+//     LOG_INFO_("\n");
+// }
 /*---------------------------------------------------------------------------*/
 
 
 
 PROCESS_THREAD(root, ev, data)
 {
-    static int err = 1;
+    // static int err = 1;
     
     PROCESS_BEGIN();
     /* Initialize DAG root */
-    NETSTACK_ROUTING.root_start();
+    // NETSTACK_ROUTING.root_start();
 
-    /* Initialize UDP connection */
-    err = simple_udp_register(&udp_conn, ROOT_DATA_PORT, NULL, AGGR_ROOTDATA_PORT, data_receiver);
-    if (err == 0) {
-      LOG_ERR("ERROR: Could not etablish data connection \n");
-    }
+    // /* Initialize UDP connection */
+    // err = simple_udp_register(&udp_conn, ROOT_DATA_PORT, NULL, AGGR_ROOTDATA_PORT, data_receiver);
+    // if (err == 0) {
+    //   LOG_ERR("ERROR: Could not etablish data connection \n");
+    // }
     
-    err = simple_udp_register(&event_conn, ROOT_EVENT_PORT, NULL, AGGR_ROOTEVENT_PORT, event_receiver);
-    if (err == 0) {
-      LOG_ERR("ERROR: Could not etablish event connection \n");
-    }
+    // err = simple_udp_register(&event_conn, ROOT_EVENT_PORT, NULL, AGGR_ROOTEVENT_PORT, event_receiver);
+    // if (err == 0) {
+    //   LOG_ERR("ERROR: Could not etablish event connection \n");
+    // }
     
-    LOG_INFO("Root: Done initialzation v.2\n");
+    LOG_INFO("Root: Done initialzation v.3\n");
 
 
     PROCESS_END();
@@ -128,7 +128,7 @@ PT_THREAD(generate_routes(struct httpd_state *s))
   PSOCK_BEGIN(&s->sout);
 
   sprintf(buff,"25,26");
-  LOG_INFO("Sending data");
+  printf("send json to requester\n");
 
   // for (int i = 0; i < next_free_name; i++)
   // {
@@ -154,14 +154,9 @@ PT_THREAD(generate_routes(struct httpd_state *s))
 
   PSOCK_END(&s->sout);
 }
+
 /*---------------------------------------------------------------------------*/
-httpd_simple_script_t
-httpd_simple_get_script(const char *name)
-{
-  return generate_routes;
-}
-/*---------------------------------------------------------------------------*/
-PROCESS(webserver, "WebServer");
+PROCESS(webserver, "Web server server");
 PROCESS_THREAD(webserver, ev, data)
 {
   PROCESS_BEGIN();
@@ -176,10 +171,18 @@ PROCESS_THREAD(webserver, ev, data)
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
+httpd_simple_script_t
+httpd_simple_get_script(const char *name)
+{
+  return generate_routes;
+}
+/*---------------------------------------------------------------------------*/
+/* Declare and auto-start this file's process */
 PROCESS(root, "root");
-PROCESS(webserver_starter, "WebServer_starter");
-AUTOSTART_PROCESSES(&root, &webserver);
+PROCESS(webserver_starter, "web server");
+AUTOSTART_PROCESSES(&root ,&webserver_starter);
 
+/*---------------------------------------------------------------------------*/
 PROCESS_THREAD(webserver_starter, ev, data)
 {
   PROCESS_BEGIN();
@@ -187,7 +190,10 @@ PROCESS_THREAD(webserver_starter, ev, data)
   PROCESS_NAME(webserver);
   process_start(&webserver, NULL);
 
-  LOG_INFO("Web Server started\n");
+  LOG_INFO("web server started++++++\n");
 
   PROCESS_END();
 }
+
+
+
